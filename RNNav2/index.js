a6cc7ed837c73b25714b31ae7e768b7bdadbeb4c
shanon3 <shanonathooya@gmail.com>
2019-10-29 23:44:59 +0700

    registering the app

--- RNNav2/index.js
+++ RNNav2/index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+import {Navigation} from 'react-native-navigation';
+import {registerScreens} from './src/screens';
+
+registerScreens();
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'Initializinf',
+      },
+    },
+  });
+});
